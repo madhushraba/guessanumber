5ee17ad2a72bcebf4cdd5bcd203b0e3eefaef35c
Madhushraba Das <75141622+madhushraba@users.noreply.github.com>
2022-01-09 12:04:24 +0530

    1stmcommit

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+let number = document.querySelector('.number')
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+
+  if (!guess) {
+    displayMessage('⛔️ No number!');
+
+  } else if (guess === secretNumber) {
+    displayMessage('🎉 Correct Number!');
+    number.textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#bb4f68 ';
+    number.style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+     displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  document.querySelector('.score').textContent = score;
+  number.textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  number.style.width = '15rem';
+});
+
